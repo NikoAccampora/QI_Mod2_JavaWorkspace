014450d33782944e9e2f362bd5fec4128c2bcc32
Nikolas Neto <nikoaccampora@gmail.com>
2021-09-06 18:04:30 -0300

    homework done

--- aula_07_trabalho2_Fisica/src/aula_07_trabalho2_Fisica/Fisica.java
+++ aula_07_trabalho2_Fisica/src/aula_07_trabalho2_Fisica/Fisica.java
@@ -2,13 +2,29 @@ package aula_07_trabalho2_Fisica;
 
 public class Fisica {
 	public float ForcaPeso(float massa,float gravidade) {
-		return massa*gravidade;
+		return massa * gravidade;
 	}
 	public float ForcaCentripeta(float massa, float velocidade, float raio) {
 		return massa * ((float) Math.pow(velocidade,2)/raio);
 	   }
 	
-	public float Impulso(float forca, float tempoInit, float tempoFin) {
-		return forca * (tempoInit - tempoFin);
+	public float Impulso(float forca, float tempo1, float tempo2) {
+		return forca * (tempo2 - tempo1);
+	   }
+	
+	public float ForcaElastica(float constElastic, float deformacao) {
+		return constElastic * deformacao;
+	   }
+	
+	public float VelocidadeMedia(float tempo1, float tempo2, float pos1, float pos2) {
+		return (float) (((pos2 - pos1) / (tempo2 - tempo1)) * 3.6);
+	   }
+	
+	public float MovimentoUniforme(float velocidade, float tempo, float posicao) {
+		return posicao + (velocidade * tempo);
+	   }
+	
+	public float MovimentoUniformeVariado(float aceleracao, float tempo, float velocidade) {
+		return velocidade + (aceleracao * tempo);
 	   }
 }
--- aula_07_trabalho2_Fisica/src/aula_07_trabalho2_Fisica/Main.java
+++ aula_07_trabalho2_Fisica/src/aula_07_trabalho2_Fisica/Main.java
@@ -16,7 +16,7 @@ public class Main {
 //		
 //		teclado.close();
 //		
-//		System.out.printf("A Força Peso é de %.2fN",calculo.ForcaPeso(massa, gravidade));
+//		System.out.printf("A Força Peso é de %.2f N",calculo.ForcaPeso(massa, gravidade));
 	    
 //		System.out.println("Ditige a massa");
 //		float massa = teclado.nextFloat();
@@ -29,20 +29,72 @@ public class Main {
 //		
 //		teclado.close();
 //		
-//		System.out.printf("A Força Centrípeta é de %.2fN",calculo.ForcaCentripeta(massa, velocidade, raio));
+//		System.out.printf("A Força Centrípeta é de %.2f N",calculo.ForcaCentripeta(massa, velocidade, raio));
 		
-		System.out.println("Ditige a força");
-		float forca = teclado.nextFloat();
+//		System.out.println("Ditige a força");
+//		float forca = teclado.nextFloat();
+//		
+//		System.out.println("Ditige o tempo inicial");
+//		float tempo1 = teclado.nextFloat();
+//		
+//		System.out.println("Ditige o tempo final");
+//		float tempo2 = teclado.nextFloat();
+//		
+//		teclado.close();
+//		
+//		System.out.printf("A grandeza do impulso é de %.2f N/s",calculo.Impulso(forca, tempo1, tempo2));
 		
-		System.out.println("Ditige o tempo inicial");
-		float tempoInit = teclado.nextFloat();
+//		System.out.println("Ditige a constante elástica");
+//		float constElastic = teclado.nextFloat();
+//		
+//		System.out.println("Ditige o nível da deformação");
+//		float deformação = teclado.nextFloat();
+//		
+//		teclado.close();
+//		
+//		System.out.printf("A força elástica é de %.2f N",calculo.ForcaElastica(constElastic, deformação));
+	    
+		System.out.println("Ditige a posição inicial em metros");
+		float pos1 = teclado.nextFloat();
 		
-		System.out.println("Ditige o tempo final");
-		float tempoFin = teclado.nextFloat();
+		System.out.println("Ditige a posição final em metros");
+		float pos2 = teclado.nextFloat();
+		
+		System.out.println("Ditige o tempo inicial em segundos");
+		float tempo1 = teclado.nextFloat();
+		
+		System.out.println("Ditige o tempo final em segundos");
+		float tempo2 = teclado.nextFloat();
 		
 		teclado.close();
 		
-		System.out.printf("O impulso é de %.2f",calculo.Impulso(forca, tempoInit, tempoFin));
+		System.out.printf("A velocidade média é de %.2f km/h",calculo.VelocidadeMedia(tempo1, tempo2, pos1, pos2));
+	    
+//	    System.out.println("Ditige a posição inicial do objeto em metros");
+//		float posicao = teclado.nextFloat();
+//	    
+//	    System.out.println("Ditige a velocidade do movimento em m/s");
+//		float velocidade = teclado.nextFloat();
+//		
+//		System.out.println("Ditige o tempo final em segundos");
+//		float tempo = teclado.nextFloat();
+//		
+//		teclado.close();
+//		
+//		System.out.printf("A função horária da posição do objeto é de %.2f m",calculo.MovimentoUniforme(velocidade, tempo, posicao));
+	    
+//	    System.out.println("Ditige a aceleração média em m/s");
+//		float aceleracao = teclado.nextFloat();
+//	    
+//	    System.out.println("Ditige a velocidade média em m/s");
+//		float velocidade = teclado.nextFloat();
+//		
+//		System.out.println("Ditige o tempo final em segundos");
+//		float tempo = teclado.nextFloat();
+//		
+//		teclado.close();
+//		
+//		System.out.printf("A função horária da velocidade é de %.2f m/s",calculo.MovimentoUniformeVariado(aceleracao, tempo, velocidade));
 	}
 
 }
