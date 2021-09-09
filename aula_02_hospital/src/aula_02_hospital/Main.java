22fb46e50907b6a2b32dd6b768aec170b69d2e70
Nikolas Neto <nikoaccampora@gmail.com>
2021-09-08 21:56:36 -0300

    exerc getters setters

--- aula_02_hospital/src/aula_02_hospital/Main.java
+++ aula_02_hospital/src/aula_02_hospital/Main.java
@@ -10,12 +10,12 @@ public class Main {
 		Paciente pac1 = new Paciente();
 		
 		//Popular os dados
-		end1.bairro = "Centro";
-		end1.cep = "0000";
-		end1.cidade = "Porto Alegre";
-		end1.estado = "RS";
-		end1.numero = "12-A";
-		end1.rua = "Travessa não te interessa";
+		end1.setBairro("Centro");
+		end1.setCEP("0000");
+		end1.setCidade("Porto Alegre");
+		end1.setEstado("RS");
+		end1.setNumero("12-A");
+		end1.setRua("Travessa não te interessa");
 		
 		pac1.cpf = "000";
 		pac1.nomeCompleto = "João Paulo Nogueiro";
@@ -25,7 +25,7 @@ public class Main {
 		
 		// Imprimir dados
 		System.out.printf("O senhor %s reside no endereço %s\n nº %s,no bairro %s - %s/%s",
-				pac1.nomeCompleto, end1.rua, end1.numero, end1.bairro, end1.cidade,end1.estado);
+				pac1.nomeCompleto, end1.getRua(), end1.getNumero(), end1.getBairro(), end1.getCidade(),end1.getEstado());
 	}
 
 }
