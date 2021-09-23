0de18a87b37e082eb2a6cb47af1112ca8b3817f9
Nikolas Neto <nikoaccampora@gmail.com>
2021-09-23 19:28:40 -0300

    N1 Update 2

--- N1_JoKenPo/src/Main.java
+++ N1_JoKenPo/src/Main.java
@@ -7,83 +7,111 @@ public class Main {
 	public static int Cscore;
 	public static int num_rounds=1;
 	
+//AJUSTE AQUI O NÚMERO MÁXIMO DE ROUNDS(0 = Rounds Infinitos)	
+	public static int max_rounds=0;
+	
 	public static void main(String[] args) {
 		Scanner typeMe = new Scanner(System.in);
 		Random randomGen = new Random();
+		
+		if(num_rounds==max_rounds) {stopGame();} else {
+		
 		System.out.printf("---=ROUND %s=---\n",num_rounds);
 		System.out.println("Faça sua jogada! Digite 1, 2 ou 3;");
 		System.out.println("1 - Pedra");
 		System.out.println("2 - Papel");
 		System.out.println("3 - Tesoura");
+		System.out.println("0 - Terminar Jogo");
 		byte playerMove = typeMe.nextByte();
 		byte comMove = (byte) (randomGen.nextInt(3)+1);
 		
-        if(playerMove==1) {
+		if(playerMove==1) {
         	if(comMove==1) {
         		System.out.println("Você Sacou PEDRA"); 
         		System.out.println("Computador Sacou PEDRA");
-        		System.out.println("EMPATE! :o");
+        		System.out.println("<< Round empatado >>");
         		num_rounds++;
-        		Main.main(args);
+        		main(args);
         	} else if(comMove==2) {
         		System.out.println("Você Sacou PEDRA"); 
         		System.out.println("Computador Sacou PAPEL");
-        		System.out.println("Você PERDEU! :(");
+        		System.out.println("<< +1 Ponto para o Computador >>");
         		num_rounds++;
-        		Main.main(args);
+        		Cscore++;
+        		main(args);
         	} else if(comMove==3) {
         		System.out.println("Você Sacou PEDRA"); 
         		System.out.println("Computador Sacou TESOURA");
-        		System.out.println("Você VENCEU! :D");
+        		System.out.println("<< +1 Ponto para Você :) >>");
         		num_rounds++;
         		Pscore++;
-        		Main.main(args);
+        		main(args);
         	} 
         } else if(playerMove==2)  {
             	if(comMove==1) {
             		System.out.println("Você Sacou PAPEL");
             		System.out.println("Computador Sacou PEDRA");
-            		System.out.println("Você VENCEU! :D");
+            		System.out.println("<< +1 Ponto para Você :) >>");
             		num_rounds++;
             		Pscore++;
-            		Main.main(args);
+            		main(args);
             	} else if(comMove==2) {
             		System.out.println("Você Sacou PAPEL");
             		System.out.println("Computador Sacou PAPEL");
-            		System.out.println("EMPATE! :o");
+            		System.out.println("<< Round empatado >>");
             		num_rounds++;
-            		Main.main(args);
+            		main(args);
             	} else if(comMove==3) {
             		System.out.println("Você Sacou PAPEL");
             		System.out.println("Computador Sacou TESOURA");
-            		System.out.println("Você PERDEU! :(");
+            		System.out.println("<< +1 Ponto para o Computador >>");
             		num_rounds++;
-            		Main.main(args);
+            		Cscore++;
+            		main(args);
             	}
             } else if(playerMove==3)  {
             	if(comMove==1) {
             		System.out.println("Você Sacou TESOURA");
             		System.out.println("Computador Sacou PEDRA");
-            		System.out.println("Você PERDEU! :(");
+            		System.out.println("<< +1 Ponto para o Computador >>");
             		num_rounds++;
-            		Main.main(args);
+            		Cscore++;
+            		main(args);
             	} else if(comMove==2) {
             		System.out.println("Você Sacou TESOURA");
             		System.out.println("Computador Sacou PAPEL");
-            		System.out.println("Você VENCEU! :D");
+            		System.out.println("<< +1 Ponto para Você :) >>");
             		num_rounds++;
             		Pscore++;
-            		Main.main(args);
+            		main(args);
             	} else if(comMove==3) {
             		System.out.println("Você Sacou TESOURA");
             		System.out.println("Computador Sacou TESOURA");
-            		System.out.println("EMPATE! :o");
+            		System.out.println("<< Round empatado >>");
             		num_rounds++;
-            		Main.main(args);
+            		main(args);
             	}
-            } else if((playerMove!=1)||(playerMove!=2)||(playerMove!=3)) {
+            } else if(playerMove==0) {
+            	stopGame();
+            } else if((playerMove!=1)||(playerMove!=2)||(playerMove!=3)||(playerMove!=0)) {
             	System.out.println("NUMERO INVÁLIDO! >:(");
-            	Main.main(args);
+            	main(args);
             }
 	} 
+	}
+	
+	public static void stopGame() {
+		System.out.println("---=!FIM DE JOGO!=---");
+		System.out.println("--Pontuação Total--");
+		System.out.printf("Você       - %s\n",Pscore);
+		System.out.printf("Computador - %s\n",Cscore);
+		if(Pscore>Cscore) {
+			System.out.println("<<< VOCÊ VENCEU!!! :D >>>");
+		} else if(Pscore<Cscore) {
+			System.out.println("<<< VOCÊ PERDEU! :'( >>>");
+		} else if(Pscore==Cscore) {
+			System.out.println("<<< EMPATE TOTAL! :O >>>");
+		} 
+		
+	}
 } 
